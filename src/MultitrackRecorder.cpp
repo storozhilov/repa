2154a0f5d8e132c5ef92c8196c133d0030410abd
Ilya Storozhilov <storozhilov@mail.ru>
2017-04-28 21:55:12 +0300

    Capture/record threads

--- src/MultitrackRecorder.cpp
+++ src/MultitrackRecorder.cpp
@@ -1,12 +1,34 @@
 #include "MultitrackRecorder.h"
 
-MultitrackRecorder::MultitrackRecorder()
+MultitrackRecorder::MultitrackRecorder() :
+	_shouldRun(false),
+	_captureThread(),
+	_recordThread()
 {}
 
 void MultitrackRecorder::start(const char * location, const char * device)
 {
+	_shouldRun = true;
+	_captureThread = boost::thread(boost::bind(&MultitrackRecorder::runCapture, this));
+	_recordThread = boost::thread(boost::bind(&MultitrackRecorder::runRecord, this));
 }
 
 void MultitrackRecorder::stop()
 {
+	_shouldRun = false;
+	_captureThread.join();
+}
+
+void MultitrackRecorder::runCapture()
+{
+	while (_shouldRun) {
+		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+	}
+}
+
+void MultitrackRecorder::runRecord()
+{
+	while (_shouldRun) {
+		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+	}
 }
--- src/MultitrackRecorder.h
+++ src/MultitrackRecorder.h
@@ -1,6 +1,9 @@
 #ifndef __SR__MULTITRACK_RECORDER_H
 #define __SR__MULTITRACK_RECORDER_H
 
+#include <boost/atomic.hpp>
+#include <boost/thread.hpp>
+
 class MultitrackRecorder
 {
 public:
@@ -8,6 +11,13 @@ public:
 
 	void start(const char * location = 0, const char * device = 0);
 	void stop();
+private:
+	void runCapture();
+	void runRecord();
+
+	boost::atomic<bool> _shouldRun;
+	boost::thread _captureThread;
+	boost::thread _recordThread;
 };
 
 #endif
