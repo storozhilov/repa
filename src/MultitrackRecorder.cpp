051b06165c2be604cf09f3c01130f5e81ee59acc
Ilya Storozhilov <storozhilov@mail.ru>
2017-05-04 23:50:05 +0300

    bytesPerSample fix & garbage removal

--- src/MultitrackRecorder.cpp
+++ src/MultitrackRecorder.cpp
@@ -232,8 +232,6 @@ template <typename Word> std::ostream& write_word(std::ostream& outs, Word value
 
 void MultitrackRecorder::runRecord(const std::string& location)
 {
-	//std::ofstream wavFile();
-
 	typedef std::vector<std::ofstream *> TargetFiles;
 	TargetFiles targetFiles;
 
@@ -245,9 +243,8 @@ void MultitrackRecorder::runRecord(const std::string& location)
 
 		~FilesCleanup()
 		{
-			// TODO Close files
+			// Closing files
 			for (std::size_t i = 0U; i < _targetFiles.size(); ++i) {
-				std::cout << "Closing " << i << "-th file" << std::endl;
 				delete _targetFiles[i];
 			}
 			_targetFiles.clear();
@@ -296,7 +293,7 @@ void MultitrackRecorder::runRecord(const std::string& location)
 			targetFiles.resize(_channels);
 			for (std::size_t i = 0U; i < _channels; ++i) {
 				std::ostringstream filename;
-				filename << "track " << i << ".wav";
+				filename << "track_" << i << ".wav";
 				std::cout << "Opening '" << filename.str() << "' file" << std::endl;
 				std::ofstream * targetFile = new std::ofstream(filename.str().c_str(),
 						std::ios_base::trunc | std::ios_base::binary);
@@ -321,34 +318,21 @@ void MultitrackRecorder::runRecord(const std::string& location)
 			}
 		}
 
-		bool debugPrinted = false;
 		// Writing data to files
 		while (!captureQueue.empty()) {
 			CaptureBuffer captureBuffer = captureQueue.front();
 
-			std::size_t bytesPerSample = 2U;	// TODO: Extract bytes per sample
 			for (std::size_t i = 0; i < _periodBufferSize; i += bytesPerSample) {
 				std::size_t frame = i / bytesPerSample / _channels;
 				std::size_t channel = i / bytesPerSample % _channels;
 				std::size_t j = channel * _periodSize * bytesPerSample + frame * bytesPerSample;
 
-				/*if (!debugPrinted) {
-					std::cout << ">>> captureBuffer.size(): " << captureBuffer.size() <<
-						", recordBuffer.size(): " << recordBuffer.size() <<
-						", _periodBufferSize: " << _periodBufferSize <<
-						", _periodSize: " << _periodSize <<
-						", i: " << i << ", frame: " << frame << ", channel: " << channel <<
-						", j: " << j << std::endl;
-				}*/
-
 				memcpy(&recordBuffer[j], &captureBuffer[i], bytesPerSample);
 			}
-			debugPrinted = true;
 
 			captureQueue.pop();
 
 			for (std::size_t i = 0U; i < targetFiles.size(); ++i) {
-				//std::cout << ">>> Writing " << _periodSize * bytesPerSample << " bytes to " << i << "-th file" << std::endl;
 				targetFiles[i]->write(&recordBuffer[i * _periodSize * bytesPerSample], _periodSize * bytesPerSample);
 			}
 		}
@@ -360,9 +344,6 @@ void MultitrackRecorder::runRecord(const std::string& location)
 			std::size_t chunkSize = file_length - 8;
 			std::size_t subchunkSize = file_length - data_chunk_pos - 8;
 
-			//std::cout << ">>> Writing " << chunkSize << "/" << subchunkSize <<
-			//	" chunk/subchunk size, file_length: " << file_length <<
-			//	", data_chunk_pos: " << data_chunk_pos << std::endl;
 			// Fix the data chunk header to contain the data size
 			targetFiles[i]->seekp(data_chunk_pos + 4);
 			write_word(*targetFiles[i], subchunkSize, 4);
@@ -372,71 +353,7 @@ void MultitrackRecorder::runRecord(const std::string& location)
 			write_word(*targetFiles[i], chunkSize, 4);
 
 			// Moving back to EOF
-			//targetFiles[i]->seekp(file_length);
 			targetFiles[i]->seekp(0, std::ios_base::end);
 		}
 	}
-/*
-	using namespace std;
-
-	namespace little_endian_io
-	{
-		template <typename Word>
-			std::ostream& write_word( std::ostream& outs, Word value, unsigned size = sizeof( Word ) )
-			{
-				for (; size; --size, value >>= 8)
-					outs.put( static_cast <char> (value & 0xFF) );
-				return outs;
-			}
-	}
-	using namespace little_endian_io;
-
-	int main()
-	{
-		ofstream f( "example.wav", ios::binary );
-
-		// Write the file headers
-		f << "RIFF----WAVEfmt ";     // (chunk size to be filled in later)
-		write_word( f,     16, 4 );  // no extension data
-		write_word( f,      1, 2 );  // PCM - integer samples
-		write_word( f,      2, 2 );  // two channels (stereo file)
-		write_word( f,  44100, 4 );  // samples per second (Hz)
-		write_word( f, 176400, 4 );  // (Sample Rate * BitsPerSample * Channels) / 8
-		write_word( f,      4, 2 );  // data block size (size of two integer samples, one for each channel, in bytes)
-		write_word( f,     16, 2 );  // number of bits per sample (use a multiple of 8)
-
-		// Write the data chunk header
-		size_t data_chunk_pos = f.tellp();
-		f << "data----";  // (chunk size to be filled in later)
-
-		// Write the audio samples
-		// (We'll generate a single C4 note with a sine wave, fading from left to right)
-		constexpr double two_pi = 6.283185307179586476925286766559;
-		constexpr double max_amplitude = 32760;  // "volume"
-
-		double hz        = 44100;    // samples per second
-		double frequency = 261.626;  // middle C
-		double seconds   = 2.5;      // time
-
-		int N = hz * seconds;  // total number of samples
-		for (int n = 0; n < N; n++)
-		{
-			double amplitude = (double)n / N * max_amplitude;
-			double value     = sin( (two_pi * n * frequency) / hz );
-			write_word( f, (int)(                 amplitude  * value), 2 );
-			write_word( f, (int)((max_amplitude - amplitude) * value), 2 );
-		}
-
-		// (We'll need the final file size to fix the chunk sizes above)
-		size_t file_length = f.tellp();
-
-		// Fix the data chunk header to contain the data size
-		f.seekp( data_chunk_pos + 4 );
-		write_word( f, file_length - data_chunk_pos + 8 );
-
-		// Fix the file header to contain the proper RIFF chunk size, which is (file size - 8) bytes
-		f.seekp( 0 + 4 );
-		write_word( f, file_length - 8, 4 ); 
-	}
-*/
 }
