0a451190984f576d8cc76c9ced9d5071b89408fc
Ilya Storozhilov <storozhilov@mail.ru>
2017-10-20 01:28:55 +0300

    GTK+ window

---
+++ vrepa/MainWindow.h
@@ -0,0 +1,15 @@
+#ifndef __VREPA__MAIN_WINDOW_H
+#define __VREPA__MAIN_WINDOW_H
+
+#include "VideoProcessor.h"
+#include <gtkmm.h>
+
+class MainWindow : public Gtk::Window
+{
+public:
+	MainWindow(VideoProcessor& vp);
+protected:
+	Gtk::VBox _vbox;
+};
+
+#endif
--- vrepa/vrepa.cpp
+++ vrepa/vrepa.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-#include <csignal>
-
+#include <gtkmm.h>
 #include <vector>
 
 #include "VideoProcessor.h"
-
-namespace
-{
-	volatile static std::sig_atomic_t stopped = static_cast<std::sig_atomic_t>(false);
-}
-
-void signal_handler(int signal)
-{
-	stopped = static_cast<std::sig_atomic_t>(true);
-}
+#include "MainWindow.h"
 
 int main(int argc, char * argv[]) {
+	Gtk::Main kit(argc, argv);
 	Gst::init(argc, argv);
 
 	guint major, minor, micro, nano;
 	Gst::version(major, minor, micro, nano);
 
 	std::cout << "GStreamer version: " << major << "." << minor << "." << micro << "." << nano << std::endl;
-
-	std::signal(SIGTERM, signal_handler);
-	std::signal(SIGINT, signal_handler);
 
 	VideoProcessor vp;
 	std::vector<VideoProcessor::SourceHandle> sources(2);
@@ -37,13 +23,9 @@ int main(int argc, char * argv[]) {
 	vp.start();
 	std::cout << "Video processor started" << std::endl;
 
-	std::size_t counter = 0;
-	while (!static_cast<bool>(stopped)) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-		std::size_t sourceIndex = counter++ % 2;
-		vp.switchSource(sources[sourceIndex]);
-	}
-	std::cout << "Termination command received -> exiting" << std::endl;
+	MainWindow window(vp);
+	Gtk::Main::run(window);
+
 	vp.stop();
 	std::cout << "Video processor stopped" << std::endl;
 }
